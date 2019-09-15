57966012ff0408ad9e9f3bdc9305749491ad9396
dada-tech <daviddadaa@hotmail.com>
2019-09-15 03:52:30 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Firestore Email Form Uploading Service
+
+HTTP POST request nuploads body parameters (after validation) to Googles Firestores DB.  
+
+# Using
+* Node
+* Express
+* Firestore
+* Firebase Cloud Functions
---
+++ functions/index.js
@@ -0,0 +1,91 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const https = require('https');
+const Joi = require('@hapi/joi');
+
+// firebase init
+admin.initializeApp(functions.config().firebase);
+let db = admin.firestore();
+
+// express middleware
+app.use(express.json());
+
+// cors middleware
+const originsWhitelist = ['https://dadadavid.com','http://localhost:4200','http://127.0.0.1:4200'];
+const corsOptions = {
+    origin: function(origin, callback) {
+        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+    },
+    "optionsSuccessStatus": 200,
+    "Access-Control-Allow-Methods":"GET, PUT, POST, DELETE, HEAD, OPTIONS",
+    "Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Origin":"*"
+}
+app.use(cors(corsOptions));
+
+// captcha
+app.post('/recaptcha',function (request, response) {
+    // verify recaptcha
+    if (request.body.recaptcha === undefined || request.body.recaptcha === '' || request.body.recaptcha === null) {
+        return response.status(400).send({"responseMsg": "Captcha undefined"});
+    }
+
+    // const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${request.body.recaptcha}&remoteip=${request.connection.remoteAddress}`;
+    const verificationURL = `https://recaptcha.google.com/recaptcha/api/siteverify?secret=${functions.config().captcha.secretkey}&response=${request.body.recaptcha}&remoteip=${request.connection.remoteAddress}`;
+
+    https.get(verificationURL, (resG) => {
+        let rawData = '';
+        resG.on('data', (chunk) => { rawData += chunk })
+        resG.on('end', function() {
+            try {
+                var parsedData = JSON.parse(rawData);
+                if (parsedData.success === true) {
+                    return response.status(200).send({"responseMsg": "Captcha success"});
+                } else {
+                    return response.status(400).send({"responseMsg": "Captcha verification Failure"});
+                }
+            } catch (e) {
+                return response.status(400).send({"responseMsg": "Captcha error"});
+            }
+        });
+    });
+});
+
+//mail POST
+app.post('/mail', (request,response) => {
+  // input validation
+  const schema = Joi.object().keys({
+      name: Joi.string().min(3).required(),
+      email: Joi.string().min(3).required(),
+      subject: Joi.string().min(3).required(),
+      message: Joi.string().min(3).required(),
+      recaptcha: Joi.string()
+  });
+
+  const result = Joi.validate(request.body,schema);
+  if(result.error) {
+      return response.status(400).send({"responseMsg": result.error.message});
+  }
+  
+  //db upload
+  db.collection("mail").add({
+      created: admin.firestore.FieldValue.serverTimestamp(),
+      name: request.body.name,
+      email: request.body.email,
+      subject: request.body.subject,
+      message: request.body.message
+  })
+      .then(() => {
+          return response.status(200).send({"responseMsg": "successfully added to database"});
+      })
+      .catch((error) => {
+          console.error(error.toString());
+          return response.status(501).send({"responseMsg": "database error"});
+      });
+});
+
+exports.app = functions.https.onRequest(app);
